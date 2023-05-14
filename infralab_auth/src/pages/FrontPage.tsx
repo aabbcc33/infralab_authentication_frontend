0b8361732f9845087c45fd974d019d0de49752af
I478554 <t.rangelov@student.fontys.nl>
2023-05-14 19:15:09 +0200

    authentication done, getting tokens

--- infralab_auth/src/pages/FrontPage.tsx
+++ infralab_auth/src/pages/FrontPage.tsx
@@ -1,8 +1,21 @@
 import '../css/FrontPage.css'
+import { generateState } from '../utils/RedirectUrlStateGenerator';
 const FrontPage = () => {
 
     const redirectButton = () => {
-        window.location.replace("https://localhost:8080/auth/oauthCallback");
+         //building out the redirect url for fhict
+         const authProviderUrl: string = "https://identity.fhict.nl/connect/authorize";
+         const client_id: string = "i476232-infralabau";
+         const scopes: string = "fhict fhict_personal openid profile roles";
+         const redirectUri: string = "https://localhost:8080/";
+         const response_type: string = "code";
+         // this parameter is added for security to prevent forgery of the url
+         const state: string = generateState();
+ 
+ 
+ 
+         
+         window.location.replace(`${authProviderUrl}?client_id=${client_id}&scope=${scopes}&redirect_uri=${redirectUri}&response_type=${response_type}&state=${state}`);
     }
 
     return (
