4e600d313a30ce8ee199df82bd40beef2231620f
Aleksandra Krasteva <I476232@fhict.nl>
2023-05-15 19:56:34 +0200

    add authenticated page

--- infralab_auth/src/App.tsx
+++ infralab_auth/src/App.tsx
@@ -1,9 +1,11 @@
+import AuthenticatedPage from './pages/AuthenticatedPage';
 import FrontPage from './pages/FrontPage';
 
 function App() {
   return (
     <div className="App">
       <FrontPage />
+    {/* <AuthenticatedPage/>  */}
     </div>
   );
 }
