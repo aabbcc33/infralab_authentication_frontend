84fcc1cc04c06d61442ad7f8bc226e8767331d72
I478554 <t.rangelov@student.fontys.nl>
2023-05-17 11:36:43 +0200

    changed html to working version

--- infralab_auth/src/pages/FrontPage.tsx
+++ infralab_auth/src/pages/FrontPage.tsx
@@ -20,23 +20,12 @@ const FrontPage = () => {
          window.location.replace(`${authProviderUrl}?client_id=${client_id}&scope=${scopes}&redirect_uri=${redirectUri}&response_type=${response_type}&state=${state}`);
     }
 
+    
     return (
-        <div className='page'>
-            <div className='background'>
-                <div className='purple-container'>
-                    <img alt='fontys-background' className='main-background' src={background} />
-                </div>
-            </div>
-            <div>
-                <div className='authenticate-container'>
-                    <img alt='fontys-icon' className='fontys-logo' src={logo} />
-                    <form  >
-                        <label>Access your infralab certificates.</label><br />
-                        <button onClick={redirectButton} type='submit'><strong>Authenticate with FHICT</strong></button>
-                    </form>
-                </div>
-            </div>
+        <div>
+            <button onClick={redirectButton} className="button-30" role="button">Authenticate with fhict</button>
         </div>
     )
+
 }
 export default FrontPage; 
