332496b9c1af3dda6310fdde22996f602998d34f
Aleksandra Krasteva <I476232@fhict.nl>
2023-05-22 20:03:47 +0200

    display certificate

--- infralab_auth/src/pages/FrontPage.tsx
+++ infralab_auth/src/pages/FrontPage.tsx
@@ -1,37 +1,43 @@
+import { useState } from 'react';
 import '../css/FrontPage.css'
 import { generateState } from '../utils/RedirectUrlStateGenerator';
 import axios from "axios";
 
 
 const FrontPage = () => {
+    const [certificate, setCertificate] = useState<string>("");
+
+
 
     const redirectButton = () => {
-         //building out the redirect url for fhict
-         const authProviderUrl: string = "https://identity.fhict.nl/connect/authorize";
-         const client_id: string = "i476232-infralabau";
-         const scopes: string = "fhict fhict_personal openid profile roles";
-         const redirectUri: string = "https://localhost:8080/";
-         const response_type: string = "code";
-         // this parameter is added for security to prevent forgery of the url
-         const state: string = generateState();
-         
-         window.location.replace(`${authProviderUrl}?client_id=${client_id}&scope=${scopes}&redirect_uri=${redirectUri}&response_type=${response_type}&state=${state}`);
+        //building out the redirect url for fhict
+        const authProviderUrl: string = "https://identity.fhict.nl/connect/authorize";
+        const client_id: string = "i476232-infralabau";
+        const scopes: string = "fhict fhict_personal openid profile roles";
+        const redirectUri: string = "https://localhost:8080/";
+        const response_type: string = "code";
+        // this parameter is added for security to prevent forgery of the url
+        const state: string = generateState();
+
+        window.location.replace(`${authProviderUrl}?client_id=${client_id}&scope=${scopes}&redirect_uri=${redirectUri}&response_type=${response_type}&state=${state}`);
     }
 
     const getCert = () => {
-        return axios.get("https://172.16.1.12:8080/certificates");
-      };
-      
+        return axios.get("https://172.16.1.12:8080/certificates").then((response) => {
+            setCertificate(response.data);
+        })
+    };
 
-    
+
     return (
         <>
-        <div>
-            <button onClick={redirectButton} className="button-30" role="button">Authenticate with fhict</button>
-        </div>
-        <div>
-            <button onClick={getCert} className="button-30" role="button">get cert?</button>
-        </div>
+            <div>
+                <button onClick={redirectButton} className="button-30" role="button">Authenticate with fhict</button>
+            </div>
+            <label>{certificate}</label>
+            <div>
+                <button onClick={getCert} className="button-30" role="button">get cert?</button>
+            </div>
         </>
     )
 
