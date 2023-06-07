f4fac94c7ccb9066a029ae53f796acb3d01dc36d
Shuhei <shuhei7196artdelta@hotmail.com>
2023-06-07 08:45:13 +0200

    add tabs

--- infralab_auth/src/pages/AdminPage.tsx
+++ infralab_auth/src/pages/AdminPage.tsx
@@ -1,18 +1,43 @@
 import { useAuth } from '../components/context/AuthProvider';
 import { Navigate, useLocation } from 'react-router-dom';
+import { Tabs, Tab } from 'react-bootstrap';
+import { useState } from 'react';
+import '../css/AdminPage.module.css'
 
-const Adminpage = () => {
+const Adminpage: React.FC = () => {
 
     const { auth } = useAuth();
     const location = useLocation();
 
-    if (!auth?.roles.includes("teacher")) {
+    const [key, setKey] = useState<string|null>('home');
+
+    if (!auth?.roles.includes("")) {
         return <Navigate to="/error" state={{ from: location }}></Navigate>
+    }
+
+    const addStudent = () => {
+
     }
 
     return (
         <>
-            <div>admin page</div>
+            <div className="trending">
+                <Tabs activeKey={key!} onSelect={(k) => setKey(k)}>
+                    <Tab 
+                        eventKey={"home"} 
+                        title="Top"
+                    >
+                    {key === 'home' && (
+                        <button onClick={addStudent} className="button-30" role="button">Add student</button>
+                    )}
+                    </Tab>
+                    <Tab eventKey={"position"} title="New">
+                    {key === 'position' && (
+                        <button onClick={addStudent} className="button-30" role="button">Add student</button>
+                    )}
+                    </Tab>
+                </Tabs>
+            </div>
         </>
     )
 }
